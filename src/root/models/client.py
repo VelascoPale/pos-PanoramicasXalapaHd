a49c13842d508de090497b067e946e36abc71f42
JorgeGabriel <paleta663@gmail.com>
2020-06-27 11:17:15 -0500

    validate is missing of register clients

--- src/root/models/client.py
+++ src/root/models/client.py
@@ -6,10 +6,11 @@ class Client(db.Model):
     idClient = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), nullable=False)
     lastname = db.Column(db.String(50), nullable=False)
-    telephone = db.Column(db.String(10)) # modificar para tomar en cuenta nuevo pedido
+    telephone = db.Column(db.String(10), nullable=False) # modificar para tomar en cuenta nuevo pedido
     email = db.Column(db.String(50))  # esta tambien
     idSchool = db.Column(db.Integer,db.ForeignKey('schools.idSchool'), nullable=False)
     group = db.Column(db.String(1))
+    
     orders_graduation = db.relationship('OrderGraduation',backref='idclient')
 
 
--- src/root/static/js/asinc-registerClients.js
+++ src/root/static/js/asinc-registerClients.js
@@ -6,40 +6,48 @@ $(document).ready(function () {
             url:'/dashboard/register/client/add',
             type:'POST',
             data:$('form').serialize(),
-            success: function(clients){
-                console.log(clients)
+            success: function(response){
+                //console.log(response)
                 $("#table").html('');
                 var output;
-                clients.forEach(client => {
-                    console.log(client['name']);
-                    output += "<tr>";
-                    output += "<td>" + client['name'] + "</td>";
-                    output += "<td class='no_visible'>" + client['lastname'] + "</td>";
-                    output += "<td class='no_visible'>" + client['telephone'] + "</td>";
-                    output += "<td class='no_visible'>" + client['email'] + "</td>";
-                    output += "<td class='no_visible'>" + client['idSchool'] + "</td>";
-                    output += "<td class='no_visible'>" + client['group'] + "</td>";
-                    output += "<td>";
-                    output += `<button type='button' class='btn btn-info btn-sm' data-toggle='modal' data-target='#exampleModalScrollable' onclick='edit_client_grd("${client[0]}","${client[1]}","${client[2]}","${client[3]}","${client[4]}","${client[5]}","${client[6]}","${client[7]}","${event}")'>Editar</button>`;
-                    //output += `<button type='button' class='btn btn-danger btn-sm ml-1' data-toggle='modal' data-target='#exampleModalScrollable' onclick='delete_client_grd("${client[0]}","${client[1]}","${client[2]}","${client[3]}","${client[4]}","${client[5]}","${client[6]}","${client[7]}","${event}")'>Eliminar</button>`;
-                    output += "</td>";
-                    output += "</tr>";
+                var alert;
+                response.forEach(function each(item, index){
+                    if (index==0){
+                        alert = '';
+                        alert += `<div class="alert ${item['type']} alert-dismissible fade show mt-3 fixed-bottom col-6 col-lg-6" role="alert" id="alerts">`;
+                        alert += `${item['text']}`;
+                        alert += '<button type="button" class="close" data-dismiss="alert" aria-label="Close">';
+                        alert += '<span aria-hidden="true">&times;</span>';
+                        alert += '</button>';
+                        alert += '</div>';
+                        $('#alerts').html(alert);
+                    }else{
+                        item.forEach(client => {
+                            output += "<tr>";
+                            output += "<td>" + client['name'] + "</td>";
+                            output += "<td>" + client['lastname'] + "</td>";
+                            output += "<td class='no_visible'>" + client['telephone'] + "</td>";
+                            output += "<td class='no_visible'>" + client['email'] + "</td>";
+                            output += "<td class='no_visible'>" + client['idSchool'] + "</td>";
+                            output += "<td class='no_visible'>" + client['group'] + "</td>";
+                            output += "<td>";
+                            output += `<button type='button' class='btn btn-info btn-sm' data-toggle='modal' data-target='#exampleModalScrollable' onclick='edit_client_grd("${client[0]}","${client[1]}","${client[2]}","${client[3]}","${client[4]}","${client[5]}","${client[6]}","${client[7]}","${event}")'>Editar</button>`;
+                            //output += `<button type='button' class='btn btn-danger btn-sm ml-1' data-toggle='modal' data-target='#exampleModalScrollable' onclick='delete_client_grd("${client[0]}","${client[1]}","${client[2]}","${client[3]}","${client[4]}","${client[5]}","${client[6]}","${client[7]}","${event}")'>Eliminar</button>`;
+                            output += "</td>";
+                            output += "</tr>";
+                            $('#table').html(output);
+                        });
+                    }
                 });
-                $('#table').html(output);
+                
             }
-        });
-    }
-
-    function clear_form(){
-        $('#clearForm').click(function(event){
-            $('#register-form')[0].reset();
         });
     }
 
     $('#register-form').submit(function(event){
         event.preventDefault();
-        clear_form();
         ajax_client();
+        document.getElementById('register-form').reset();
     });
 
 });
